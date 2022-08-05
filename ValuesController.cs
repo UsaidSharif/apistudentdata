d760b000325244caadb332a1940131a8544f22ee
UsaidSharif <108445550+UsaidSharif@users.noreply.github.com>
2022-08-05 14:36:22 +0500

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.Script.Serialization;
+using System.Runtime.Caching;
+
+
+namespace API_StudentTable.Controllers
+{
+    public class ValuesController : ApiController
+    {
+       
+        public class Students
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public int Marks { get; set; }
+
+            public Students(int id, string name, string address, int marks)
+            {
+                this.ID = id;
+                this.Name = name;
+                this.Address = address;
+                this.Marks = marks;
+            }
+
+        }
+        // GET api/values
+        public string Get()
+        {
+            string connetionString = ConfigurationManager.ConnectionStrings["ProjectAPI"].ConnectionString; ;
+            SqlConnection mycnn;
+
+            mycnn = new SqlConnection(connetionString);
+            mycnn.Open();
+            SqlCommand command;
+            SqlDataReader dataReader;
+            string sql = "Select * From StudentData";
+            command = new SqlCommand(sql, mycnn);
+            dataReader = command.ExecuteReader();
+
+            List<Students> StudentList = new List<Students>();
+
+            while (dataReader.Read())
+            {
+                int ID = Convert.ToInt32(dataReader.GetValue(0));
+                string name = dataReader.GetValue(1).ToString();
+                string address = dataReader.GetValue(2).ToString();
+                int marks = Convert.ToInt32(dataReader.GetValue(3));
+                Students account = new Students(ID, name, address, marks);
+                StudentList.Add(account);
+            }
+
+            dataReader.Close();
+            command.Dispose();
+            mycnn.Close();
+
+            ObjectCache cache = MemoryCache.Default;
+
+            if (cache["students"] == null)
+            {
+                var cacheItemPolicy = new CacheItemPolicy
+                {
+                    AbsoluteExpiration = DateTimeOffset.Now.AddDays(1)
+                };
+                var cacheItem = new CacheItem("students", StudentList);
+                cache.Add(cacheItem, cacheItemPolicy);
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+
+            return js.Serialize((List<Students>)cache.Get("students"));
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebForm1.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="API_StudentTable.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+        crossorigin="anonymous">
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css" />
+    <script type="text/javascript" src="//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+
+    <title></title>
+     <script type="text/javascript">
+        $(document).ready(function () {
+            //$('#datatable').dataTable();
+            $.ajax(
+                {
+                    url: 'api/values',
+                    method: 'get',
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    async: false,
+                    success: function (data) {
+                        $('#datatable').dataTable({
+                            data: JSON.parse(data),
+                            columns: [
+                                { 'data': 'ID' },
+                                { 'data': 'Name' },
+                                { 'data': 'Address' },
+                                { 'data': 'Marks' }
+                            ]
+                        });
+                    }
+                    
+                });
+        });
+       
+    </script>
+</head>
+<body style="font-family: Arial">
+    <form id="form1" runat="server">
+        <div style="width: 500px; border: 1px solid black; padding: 5px">
+            
+            <table  id="datatable" >
+                <thead>
+                <tr>
+                   <th>ID</th>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Address</th>
+                    <th>Marks</th>
+                </tr>
+                </thead>
+            </table>
+            
+        </div>
+    </form>
+</body>
+</html>
